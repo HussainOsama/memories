e3ee4c336f7029ed22622ac310bdeb4456cfcba3
HussainOsama <hussain.osama.asd@gmail.com>
2021-03-22 21:32:00 +0300

    To delete

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,15 +1,8 @@
-import postMassage from "../models/postMassage.js";
-
-const main = (req, res) => {
-  res.send("Hello worlds");
-};
-
-const about = (req, res) => {
-  res.send("Welcome to ABOUT page ");
-};
+import postMessage from "../models/postMessage.js";
+import mongoose from "mongoose";
 
 const getPosts = async (req, res) => {
-  const posts = await postMassage.find({});
+  const posts = await postMessage.find({});
 
   try {
     res.json(posts);
@@ -19,7 +12,7 @@ const getPosts = async (req, res) => {
 };
 
 const createPost = async (req, res) => {
-  const newPost = new postMassage(req.body);
+  const newPost = new postMessage(req.body);
 
   try {
     await newPost.save();
@@ -31,7 +24,7 @@ const createPost = async (req, res) => {
 
 const deletePost = async (req, res) => {
   try {
-    const post = await postMassage.findByIdAndDelete(req.params.id);
+    const post = await postMessage.findByIdAndDelete(req.params.id);
 
     if (!post) res.status(404).send("Post not found");
     res.status(200).send();
@@ -40,4 +33,51 @@ const deletePost = async (req, res) => {
   }
 };
 
-export { main, about, getPosts, createPost, deletePost };
+const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { title, message, creator, selectedFile, tags } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+
+  const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+  await postMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+  res.json(updatedPost);
+};
+
+export { getPosts, createPost, deletePost, updatePost };
+
+// const updatePost = async (req, res) => {
+//   const { id } = req.params;
+//   // const { title, message, creator, selectedFile, tags } = req.body;
+//   const post = req.body;
+//   if (!mongoose.Types.ObjectId.isValid(id)) {
+//     return res.status(404).send(`No post with id: ${id}`);
+//   }
+
+//   try {
+//     console.log("Hello Update try");
+//     // const updatedPost = {
+//     //   creator,
+//     //   title,
+//     //   message,
+//     //   tags,
+//     //   selectedFile,
+//     //   _id: id,
+//     // };
+//     // console.log(updatedPost.creator);
+//     const updated = await postMessage.findByIdAndUpdate(
+//       id,
+//       { ...post, _id },
+//       {
+//         new: true,
+//       }
+//     );
+//     console.log(updated);
+//     res.json(updated);
+//   } catch (err) {
+//     res.send(err);
+//   }
+// };
--- server/router/router.js
+++ server/router/router.js
@@ -1,24 +1,18 @@
 import express from "express";
 import {
-  main,
-  about,
   getPosts,
   createPost,
   deletePost,
+  updatePost,
 } from "../controller/controller.js";
 // const main = require("../controller/controller.js");
 var router = express.Router();
 
 // middleware that is specific to this router
 
-router.get("/", main);
-
-router.get("/about", about);
-
 router.get("/posts", getPosts);
-
 router.post("/posts", createPost);
-
 router.delete("/posts/:id", deletePost);
+router.patch("/posts/:id", updatePost);
 
 export default router;
